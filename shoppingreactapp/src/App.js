733d6d98467faeb3e51d705d0d79d2e385f9413a
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-10-23 11:32:30 +0400

    Ecommerce Single Product Page

--- shoppingreactapp/src/App.js
+++ shoppingreactapp/src/App.js
@@ -1,4 +1,5 @@
 import Home from "./Pages/Home";
+import Product from "./Pages/Product";
 import ProductList from "./Pages/ProductList";
 
 
@@ -6,7 +7,7 @@ import ProductList from "./Pages/ProductList";
 function App() {
   return (
     <>
-   <ProductList/>
+   <Product/>
     </>
 
   );
